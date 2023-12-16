d8ba4ac814b9706053834bc56a6ae0f0aaced9ca
Serghei <akaindark@gmail.com>
2023-12-16 08:48:21 +0200

    fix task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,3 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
-number1 % 2===0? console.log(number1 + ' ' + 'четное число'): console.log(number1 + ' ' + 'не четное число')
+number1 % 2 === 0 ? console.log(number1, 'четное число'): console.log(number1, 'не четное число')
